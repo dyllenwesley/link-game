3c7fce90d10b7d7ccdc2881a4b21ad94ec2dda6d
Dyllen Holm <>
2018-04-04 12:48:41 -0600

    Removed Hard coded Heros

--- game.js
+++ game.js
@@ -17,14 +17,11 @@ function main(){
     windowSetup();
 
     document.getElementById("canvasbox").appendChild(canvas);
-    /*myhero = new HeroAnimate(30, 30);
+    myhero = new HeroAnimate(30, 30);
     myhero1 = new HeroAnimate(280, 30);
     myhero2 = new HeroAnimate(30, 280);
-    myhero3 = new HeroAnimate(280, 280);*/
-    myhero = new Hero;
-    myhero1 = new Hero1;
-    myhero2 = new Hero2;
-    myhero3 = new Hero3;
+    myhero3 = new HeroAnimate(280, 280);
+
     loadGraghics();
 
 
@@ -44,7 +41,7 @@ function canvasSetup(){
     renderingContext = canvas.getContext("2d");
 }
 
-/*function HeroAnimate(x, y) {
+function HeroAnimate(x, y) {
     this.x = x;
     this.y = y;
     this.frame = 0;
@@ -64,111 +61,6 @@ function canvasSetup(){
         renderingContext.save();
         var n = this.blinkAnimation[this.frame];
         SpriteArray[n].draw(renderingContext, this.x, this.y);
-
-        renderingContext.restore()
-    }
-}*/
-
-
-function Hero() {
-    this.x = 30;
-    this.y = 30;
-    this.frame = 0;
-    this.health = 100;
-    this.gravity = 1.25;
-    this.velocity = 0;
-    this.jump = 4.6;
-    this.blinkAnimation = [0,1,2,3,4,5,6,7,6];
-
-    this.update = function () {
-        var n = 7;
-        this.frame += frames % n === 0 ? 1:0;
-        this.frame %= this.blinkAnimation.length;
-    };
-
-    this.draw = function(renderingContext) {
-        renderingContext.save();
-
-        var n = this.blinkAnimation[this.frame];
-        linkBlink[n].draw(renderingContext, this.x, this.y);
-
-        renderingContext.restore()
-    }
-}
-
-function Hero1() {
-    this.x = 280;
-    this.y = 30;
-    this.frame = 0;
-    this.health = 100;
-    this.gravity = 1.25;
-    this.velocity = 0;
-    this.jump = 4.6;
-    this.blinkAnimation = [0,1,2,3,4,5,6,7,6];
-
-    this.update = function () {
-        var n = 7;
-        this.frame += frames % n === 0 ? 1:0;
-        this.frame %= this.blinkAnimation.length;
-    };
-
-    this.draw = function(renderingContext) {
-        renderingContext.save();
-
-        var n = this.blinkAnimation[this.frame];
-        linkBlink1[n].draw(renderingContext, this.x, this.y);
-
-        renderingContext.restore()
-    }
-}
-
-function Hero2() {
-    this.x = 30;
-    this.y = 280;
-    this.frame = 0;
-    this.health = 100;
-    this.gravity = 1.25;
-    this.velocity = 0;
-    this.jump = 4.6;
-    this.blinkAnimation = [0,1,2,3,4,5,6,7,6];
-
-    this.update = function () {
-        var n = 7;
-        this.frame += frames % n === 0 ? 1:0;
-        this.frame %= this.blinkAnimation.length;
-    };
-
-    this.draw = function(renderingContext) {
-        renderingContext.save();
-
-        var n = this.blinkAnimation[this.frame];
-        linkBlink2[n].draw(renderingContext, this.x, this.y);
-
-        renderingContext.restore()
-    }
-}
-
-function Hero3() {
-    this.x = 280;
-    this.y = 280;
-    this.frame = 0;
-    this.health = 100;
-    this.gravity = 1.25;
-    this.velocity = 0;
-    this.jump = 4.6;
-    this.blinkAnimation = [0,1,2,3,4,5,6,7,6];
-
-    this.update = function () {
-        var n = 7;
-        this.frame += frames % n === 0 ? 1:0;
-        this.frame %= this.blinkAnimation.length;
-    };
-
-    this.draw = function(renderingContext) {
-        renderingContext.save();
-
-        var n = this.blinkAnimation[this.frame];
-        linkBlink3[n].draw(renderingContext, this.x, this.y);
 
         renderingContext.restore()
     }
