b6a69acf8969de209117a0419e32a8dea3fa2805
Dyllen Holm <>
2018-04-15 14:24:07 -0600

    no message

--- game.js
+++ game.js
@@ -90,7 +90,7 @@ function Hero(){
 
     this.moveAnimation = [0,1,2,3,4,5,6,7,8,9];
 
-    this.blinkAnimation = [0,1,2,1];
+    this.blinkAnimation = [0,0,0,0,0,0,0,0,0,0,0,1,2,1];
 
     this.jump = function(){
         this.stop = false;
